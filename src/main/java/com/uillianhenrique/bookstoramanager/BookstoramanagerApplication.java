4cb0949182e065a7e7a07a23898aa2ffc2ccece5
uillianamaral <uillianhenrique@hotmail.com>
2021-01-05 16:08:04 -0300

    firt commit project structure created

---
+++ src/main/java/com/uillianhenrique/bookstoramanager/BookstoramanagerApplication.java
@@ -0,0 +1,13 @@
+package com.uillianhenrique.bookstoramanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoramanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoramanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uillianhenrique/bookstoramanager/BookstoramanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uillianhenrique.bookstoramanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoramanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
